00241bc619b5deb6b3de44116943f2e7b700a393
Anatolii-sx <s8.8@bk.ru>
2021-11-08 20:51:13 +0300

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Анатолий Миронов on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
